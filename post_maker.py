8396625baf33985834b62445573e844a9090e0bf
Laird <LairdStreak@gmail.com>
2019-12-24 17:56:43 +1300

    Use Python Black

--- post_maker.py
+++ post_maker.py
@@ -7,15 +7,16 @@ import datetime
 def new_post():
     """Creates a empty post file"""
     currentDT = datetime.datetime.now()
-    title = 'Title: Blank Post'
-    dtvalue = 'Date: {}'.format(currentDT.strftime("%Y-%m-%d %H:%M"))
-    cat = 'Category: Holder'
-    tags = 'Tags: Holder'
-    summary = 'Summary: summary.'
+    title = "Title: Blank Post"
+    dtvalue = "Date: {}".format(currentDT.strftime("%Y-%m-%d %H:%M"))
+    cat = "Category: Holder"
+    tags = "Tags: Holder"
+    summary = "Summary: summary."
     line_list = [title, dtvalue, cat, tags, summary]
-    with open('content/post_template.md', 'w') as writer:
+    with open("content/post_template.md", "w") as writer:
         for item in line_list:
             writer.write("%s\n" % item)
 
+
 if __name__ == "__main__":
-    new_post()            
+    new_post()
